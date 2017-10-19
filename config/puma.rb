9124c15b642c4993c294b75c56052fa61378f804
ukaji21 <ukaji21@gmail.com>
2017-10-19 11:26:59 +0900

    puma socket設定追加

--- config/puma.rb
+++ config/puma.rb
@@ -54,3 +54,5 @@ environment ENV.fetch("RAILS_ENV") { "development" }
 
 # Allow puma to be restarted by `rails restart` command.
 plugin :tmp_restart
+
+bind 'unix:///var/www/coach_matching/tmp/sockets/puma.sock'
